406879e73b57faa34058025386610e79e6472ab1
Şeyma Çakır <cakirs2001@gmail.com>
2026-03-23 11:59:04 +0300

    FIX

--- custom_components/desi/const.py
+++ custom_components/desi/const.py
@@ -10,7 +10,7 @@ LOGIN_METHODS = ["phone", "email"]
 DEFAULT_LOGIN_METHOD = "email"
 
 
-AUTH_URI = "https://desismart.io/api/third_part_devices/ds/home_assistant/oauth/authorize"
+AUTH_URI = "https://web.desismart.io/ds/sign-in-for-home-assistant"
 TOKEN_URI = "https://desismart.io/api/third_part_devices/ds/home_assistant/token"
 FULLFILMENT_API_URI = (
     "https://desismart.io/api/third_part_devices/ds/home_assistant/control"
