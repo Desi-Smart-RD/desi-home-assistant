c8a9e283accf382255b7bf7ddc97ede15d88e2fd
Şeyma Çakır <cakirs2001@gmail.com>
2026-03-23 15:07:54 +0300

    REFACTOR

--- custom_components/desi/config_flow.py
+++ custom_components/desi/config_flow.py
@@ -6,10 +6,25 @@ import jwt  # type: ignore  # noqa: PGH003
 
 from homeassistant.config_entries import ConfigFlowResult
 from homeassistant.helpers import config_entry_oauth2_flow
+from homeassistant.helpers.network import get_url
 
 from .const import AUTH_URI, DOMAIN, TOKEN_URI
 
 _LOGGER = logging.getLogger(__name__)
+
+
+class DesiLocalOAuth2Implementation(
+    config_entry_oauth2_flow.LocalOAuth2ImplementationWithPkce
+):
+    """Get redirect uri for callback."""
+
+    @property
+    def redirect_uri(self) -> str:
+        """Get users network address for redirict."""
+        base_url = get_url(self.hass)
+        # Hata ayıklama için burayı error seviyesinde logluyoruz
+        _LOGGER.info("Redirect Uri: %s", base_url)
+        return f"{base_url}/auth/external/callback"
 
 
 class DesiConfigFlow(config_entry_oauth2_flow.AbstractOAuth2FlowHandler, domain=DOMAIN):
@@ -24,36 +39,51 @@ class DesiConfigFlow(config_entry_oauth2_flow.AbstractOAuth2FlowHandler, domain=
         return _LOGGER
 
     async def async_step_user(self, user_input=None) -> ConfigFlowResult:
-        """Handle a flow initialized by the user from the integrations page."""
+        """Handle a flow initialized by the user."""
 
-        # Check for registered OAuth implementations
         implementations = await config_entry_oauth2_flow.async_get_implementations(
             self.hass, DOMAIN
         )
 
-        if not implementations:
+        self.flow_impl = next(
+            (
+                impl
+                for impl in implementations.values()
+                if isinstance(impl, DesiLocalOAuth2Implementation)
+            ),
+            None,
+        ) # type: ignore  # noqa: PGH003
+
+        if self.flow_impl is None:
             config_entry_oauth2_flow.async_register_implementation(
                 self.hass,
                 DOMAIN,
-                config_entry_oauth2_flow.LocalOAuth2Implementation(
-                    self.hass, DOMAIN, "", "", AUTH_URI, TOKEN_URI
+                DesiLocalOAuth2Implementation(
+                    self.hass,
+                    DOMAIN,
+                    "",
+                    AUTH_URI,
+                    TOKEN_URI,
+                    "",
+                    128,
                 ),
             )
-
-            # Refresh the list of implementations after registration
             implementations = await config_entry_oauth2_flow.async_get_implementations(
                 self.hass, DOMAIN
             )
-
-        self.flow_impl = list(implementations.values())[0]
+            self.flow_impl = next(
+                    impl
+                    for impl in implementations.values()
+                    if isinstance(impl, DesiLocalOAuth2Implementation)
+            )
 
         # Redirect the user directly to the OAuth authentication page
         return await self.async_step_auth()
 
     async def async_oauth_create_entry(self, data: dict) -> ConfigFlowResult:
-        """Create an entry after successful OAuth authentication and token retrieval."""
+        """Create an entry after successful OAuth authentication."""
         try:
-            # Extract the access token from the response data
+
             token_data = data["token"]["access_token"]
             decoded = jwt.decode(token_data, options={"verify_signature": False})
             user_id = decoded["sub"]
