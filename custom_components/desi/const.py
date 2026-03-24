fe6b7c1a24f52b08284ed8873db177dfcd209cf7
Şeyma Çakır <cakirs2001@gmail.com>
2026-03-24 09:23:09 +0300

    UPDATE

--- custom_components/desi/const.py
+++ custom_components/desi/const.py
@@ -10,12 +10,12 @@ LOGIN_METHODS = ["phone", "email"]
 DEFAULT_LOGIN_METHOD = "email"
 
 
-AUTH_URI = "https://web.desisteely.com/ds/sign-in-for-home-assistant"
-TOKEN_URI = "https://desisteely.com/api/third_part_devices/ds/home_assistant/token"
+AUTH_URI = "https://web.desismart.io/ds/sign-in-for-home-assistant"
+TOKEN_URI = "https://desismart.io/api/third_part_devices/ds/home_assistant/token"
 FULLFILMENT_API_URI = (
-    "https://desisteely.com/api/third_part_devices/ds/home_assistant/control"
+    "https://desismart.io/api/third_part_devices/ds/home_assistant/control"
 )
-API_URL = "https://desisteely.com"
+API_URL = "https://desismart.io"
 SOCKET_PATH = "/api/third_part_devices/ds/home_assistant/ws"
 WS_URL = API_URL + SOCKET_PATH
 
